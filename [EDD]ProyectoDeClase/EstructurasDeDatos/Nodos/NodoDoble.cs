30c26b246c2258e707d62c88b31a8e64f7d94be1
Javier Antonio Alvarez Gonzalez <javier3448.ja@gmail.com>
2019-11-12 17:40:42 -0600

    primer commit

---
+++ [EDD]ProyectoDeClase/EstructurasDeDatos/Nodos/NodoDoble.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _EDD_ProyectoDeClase.EstructurasDeDatos.Nodos
+{
+    public class NodoDoble<T>
+    {
+        public T Value { get; set; }
+        public NodoSimple<T> Prev { get; set; }
+        public NodoSimple<T> Next { get; set; }
+    }
+}
